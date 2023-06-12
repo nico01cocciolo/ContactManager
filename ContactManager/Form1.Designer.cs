d8323e4e4241c3ac417a1e39db66d5cc1a7fee52
nico01cocciolo <nicocciolo01@gmail.com>
2023-06-12 16:46:13 +0200

    Mindestfenstergrösse

--- ContactManager/Form1.Designer.cs
+++ ContactManager/Form1.Designer.cs
@@ -74,7 +74,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
