cb078d0de909afa1b4cab1000cd2700add178151
Schneggo <fabio13.schegg.gmail.com@zbw-online.ch>
2023-06-12 16:33:05 +0200

    Added MasterClass "Person" Added Subclasses "Mitarbeiter", "Lehrling", "Kunde"

---
+++ ContactManager/Model/Kunde.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager.Model
+{
+    internal class Kunde : Person
+    {
+        //ADD
+        /* Firmenname, 
+         * Geschäftsadresse, 
+         * Kundentyp (A-E), 
+         * Firmenkontakt 
+         */
+    }
+}
---
+++ ContactManager/Model/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager.Model
+{
+    internal class Person
+    {
+        //ADD
+        /*  Anrede
+            Vorname
+            Nachname
+            Geburtstdatum
+            Geschlecht
+            Titel
+            Telefonnummer
+            Geschäft
+            Mobiltelefonnummer
+            E-Mail
+            Telefonnummer Geschäft
+            Status (aktiv,passiv)
+            Wohnort
+            Nationalität
+        */
+
+    }
+}
